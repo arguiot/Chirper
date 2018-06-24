d7d60d5a81a14331fdafc71fbbbc8df6c80a5005
Arthur Guiot <arguiot@gmail.com>
2018-06-24 11:37:04 +0800

    Starting to design

--- Chirper/AppDelegate.swift
+++ Chirper/AppDelegate.swift
@@ -12,34 +12,26 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
 
+    @IBOutlet weak var menu: NSMenu!
+    var WC: NSWindowController?
     let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         if let button = statusItem.button {
             button.image = NSImage(named:NSImage.Name("Chirper"))
-            button.action = #selector(printQuote(_:))
         }
-        constructMenu()
+        statusItem.menu = self.menu
     }
-
+    @IBAction func OpenWindow(_ sender: Any) {
+        let window = NSStoryboard(name: "Main", bundle: nil).instantiateController(withIdentifier: "ChirperWindow") as! NSWindowController
+        WC = window
+        WC?.showWindow(self)
+        print(WC?.window?.title)
+    }
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
-    }
-    func constructMenu() {
-        let menu = NSMenu()
-        
-        menu.addItem(NSMenuItem(title: "Print Quote", action: #selector(AppDelegate.printQuote(_:)), keyEquivalent: "P"))
-        menu.addItem(NSMenuItem.separator())
-        menu.addItem(NSMenuItem(title: "Quit Quotes", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
-        
-        statusItem.menu = menu
-    }
-    @objc func printQuote(_ sender: Any?) {
-        let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
-        let quoteAuthor = "Mark Twain"
-        
-        print("\(quoteText) — \(quoteAuthor)")
     }
 
 }
