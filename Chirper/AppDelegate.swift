402446ec6f401d123afb3f464671a3867cf7b989
Arthur Guiot <arguiot@gmail.com>
2018-06-24 09:15:47 +0800

    Initial Commit

---
+++ Chirper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Chirper
+//
+//  Created by Arthur Guiot on 24/06/2018.
+//  Copyright © 2018 Arthur Guiot. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
