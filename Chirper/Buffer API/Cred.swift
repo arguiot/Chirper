b300f2bd41348c245e1da29396c2e68e13361807
Arthur Guiot <arguiot@gmail.com>
2018-06-27 08:36:04 +0800

    Added creds

---
+++ Chirper/Buffer API/Cred.swift
@@ -0,0 +1,19 @@
+//
+//  Cred.swift
+//  Chirper
+//
+//  Created by Arthur Guiot on 26/06/2018.
+//  Copyright © 2018 Arthur Guiot. All rights reserved.
+//
+
+import Foundation
+
+class BufferCred {
+    let BUFFER_CLIENT_ID = "5b2f84ea4f87a6af7d185461"
+    let BUFFER_CLIENT_SECRET = "e801eefcf1ff49c27bea9e2587caa238"
+    let redirectURI = "https://arguiot.github.io/Chirper/api"
+    let requestURL: String?
+    init() {
+        requestURL = "https://bufferapp.com/oauth2/authorize?client_id=\(BUFFER_CLIENT_ID)&redirect_uri=\(redirectURI)&response_type=code"
+    }
+}
