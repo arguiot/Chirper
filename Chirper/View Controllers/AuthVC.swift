df688302e0010b4fcb6a820232a820bc017602c4
Arthur Guiot <arguiot@gmail.com>
2018-06-27 08:36:25 +0800

    Added AuthVC with WebView

---
+++ Chirper/View Controllers/AuthVC.swift
@@ -0,0 +1,44 @@
+//
+//  AuthVC.swift
+//  Chirper
+//
+//  Created by Arthur Guiot on 26/06/2018.
+//  Copyright © 2018 Arthur Guiot. All rights reserved.
+//
+
+import Cocoa
+import WebKit
+
+class AuthVC: NSViewController, WKUIDelegate, WKNavigationDelegate {
+
+    @IBOutlet weak var WebView: WKWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+        
+        WebView.uiDelegate = self
+        WebView.navigationDelegate = self
+        
+        
+        self.WebView.addObserver(self, forKeyPath: "URL", options: .new, context: nil)
+        self.WebView.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: nil)
+        
+        let creds = BufferCred();
+        let url = URL(string: creds.requestURL!)
+        let request = URLRequest(url: url!)
+        
+        WebView.load(request)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == #keyPath(WKWebView.estimatedProgress) {
+            // When page load finishes. Should work on each page reload.
+            if (self.WebView.estimatedProgress == 1) {
+                print("### URL:", self.WebView.url)
+            }
+        }
+    }
+    @IBAction func cancel(_ sender: Any) {
+        
+    }
+}
